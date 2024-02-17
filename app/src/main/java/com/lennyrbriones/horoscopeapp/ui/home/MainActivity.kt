64faa2697baf301cdc2d40d6b8f734e67b8e120d
LennyRBriones <lennyrbriones@outlook.com>
2024-02-17 01:47:23 -0600

    set id, configure navcontroller

--- app/src/main/java/com/lennyrbriones/horoscopeapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/lennyrbriones/horoscopeapp/ui/home/MainActivity.kt
@@ -2,6 +2,10 @@ package com.lennyrbriones.horoscopeapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.lennyrbriones.horoscopeapp.R
 import com.lennyrbriones.horoscopeapp.databinding.ActivityMainBinding
 
@@ -10,9 +14,22 @@ class MainActivity : AppCompatActivity() {
     //Setting ViewBinding
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController //navigation controller
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
