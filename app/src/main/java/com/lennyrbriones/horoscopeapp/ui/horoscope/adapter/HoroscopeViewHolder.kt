48786a2c00b3570bd09b039d6ba4e3db1cb2369c
LennyRBriones <lennyrbriones@outlook.com>
2024-02-19 20:44:41 -0600

    set animation with second lambda

--- app/src/main/java/com/lennyrbriones/horoscopeapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
+++ app/src/main/java/com/lennyrbriones/horoscopeapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -1,6 +1,7 @@
 package com.lennyrbriones.horoscopeapp.ui.horoscope.adapter
 
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import com.lennyrbriones.horoscopeapp.databinding.ItemHoroscopeBinding
 import com.lennyrbriones.horoscopeapp.domain.model.HoroscopeInfo
@@ -15,6 +16,21 @@ class HoroscopeViewHolder(view: View):RecyclerView.ViewHolder(view) {
         binding.ivHoroscope.setImageResource(horoscopeInfo.img)
         binding.tvtitle.text = context.getString(horoscopeInfo.name)
 
-        binding.parent.setOnClickListener {onItemselected(horoscopeInfo)}
+        binding.parent.setOnClickListener {
+            //calling the second lambda to summon up the first lambda with the data
+        startRotationAnimation(binding.ivHoroscope, secondLambda = {onItemselected(horoscopeInfo)})
+        //onItemselected(horoscopeInfo)
+        }
+    }
+
+    //generating the animation
+    private fun startRotationAnimation(view: View, secondLambda:() -> Unit){ //second lambda
+        view.animate().apply {
+            duration = 500 // in milliseconds
+            interpolator = LinearInterpolator()
+            rotationBy(360f)
+            withEndAction{ secondLambda() } //setting at the end of action
+            start()
+        }
     }
 }
