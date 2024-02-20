5da3c005bf9d6b39e121bd2e9ae1f427421545b2
LennyRBriones <lennyrbriones@outlook.com>
2024-02-19 23:09:38 -0600

    setting binding,injection & view model in new activity

---
+++ app/src/main/java/com/lennyrbriones/horoscopeapp/ui/detail/HoroscopeDetailActivity.kt
@@ -0,0 +1,21 @@
+package com.lennyrbriones.horoscopeapp.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.lennyrbriones.horoscopeapp.R
+import com.lennyrbriones.horoscopeapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class HoroscopeDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val horoscopeDetailViewModel:HoroscopeDetailViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
